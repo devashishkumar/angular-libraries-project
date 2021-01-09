f027619fa6022a0ed6035c1149062981029b4a6f
arora.ashish0512 <arora.ashish0512@gmail.com>
2021-01-09 13:57:26 +0530

    use google-maps-angular library from npm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { GoogleMapsAngularComponent} from 'google-maps-angular';
 declare var google: any;
 
 @Component({
@@ -8,13 +9,20 @@ declare var google: any;
 })
 export class AppComponent implements OnInit {
   title = 'ashish-gmap-library';
-  lat: any = 28.5636;
-  long: any = 77.3726;
+  // lat: any = 28.5636;
+  // long: any = 77.3726;
+  // noida sector 49 lat/long
+  // latLong: any = {
+  //   lat: 28.5636,
+  //   long: 77.3726
+  // };
+  // kanpur govind nagar lat/long
   latLong: any = {
-    lat: 28.5636,
-    long: 77.3726
+    lat: 26.4492,
+    long: 80.3094
   };
   nMap: any = {};
+  @ViewChild('googleComponent') googleMapCompObj: GoogleMapsAngularComponent
 
   ngOnInit() {
   }
