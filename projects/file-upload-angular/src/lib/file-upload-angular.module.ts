a5816048c58ec2a0aa3f229405185ffc20f03da9
arora.ashish0512 <arora.ashish0512@gmail.com>
2021-01-26 13:52:30 +0530

    add forms module in file upload library module

--- projects/file-upload-angular/src/lib/file-upload-angular.module.ts
+++ projects/file-upload-angular/src/lib/file-upload-angular.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { FileUploadAngularComponent } from './file-upload-angular.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [FileUploadAngularComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [FileUploadAngularComponent]
 })
